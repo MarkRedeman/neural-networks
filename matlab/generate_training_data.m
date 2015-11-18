94f6a2b570b6035063673b68dd1debeb8889b53d
Mark Redeman <markredeman@gmail.com>
2015-11-18 01:48:03 +0100

    [Initial Commit] Add generate_training_data function

---
+++ matlab/generate_training_data.m
@@ -0,0 +1,12 @@
+%% generate_training_data:
+function [labels, vectors] = generate_training_data(N, P)
+    labels = generate_labels(P);
+    vectors = randn(P, N);
+end
+
+%% generate_labels:
+function [labels] = generate_labels(P)
+    % Create P labels equal to plus / minus 1 with 0.5 probability
+    labels = randi([0, 1], P, 1);
+    labels(find(labels == 0)) = -1;
+end
