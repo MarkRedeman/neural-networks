0f44cc742494e6dbc51c1a2794c890db4fcf7252
Mark Redeman <markredeman@gmail.com>
2015-12-31 22:30:45 +0100

    Use new test structure

--- matlab/test.m
+++ matlab/test.m
@@ -1,20 +1,43 @@
 %% test:
 % Runs each test in the current folder and includes files from the src
 % folder before running them
-function [result] = test()
+function [result] = test(ci)
+    if (nargin < 1)
+        ci = false;
+    end
+
+    try
+        p = addpath(pwd);
+        results = run_tests();
+    catch e
+        path(p);
+        disp(getReport(e,'extended'));
+        if (ci)
+            exit(1);
+        end
+    end
+
+    if (ci)
+        exit(any([results.Failed]));
+    end
+end
+
+%% run_tests:
+function [results] = run_tests()
     clear functions;
 
-    whatToTest = '../';
-
     import matlab.unittest.TestSuite
+    import matlab.unittest.TestRunner;
+    import matlab.unittest.plugins.CodeCoveragePlugin;
 
-    suite = TestSuite.fromFolder('./tests');
+    runner = TestRunner.withTextOutput;
 
-    % Add the files under test to our path
-    addpath(whatToTest);
+    % Since the function name of a test described the tests' behavior we don't want to limit
+    % the length of a function name
+    warning off MATLAB:namelengthmaxexceeded;
+    % suite = TestSuite.fromFolder('tests', 'IncludingSubfolders', true);
+    suite = TestSuite.fromPackage('tests', 'IncludingSubpackages', true);
 
-    result = run(suite);
-
-    % Remove files under test from our path
-    rmpath(whatToTest);
+    results = runner.run(suite);
+    warning on MATLAB:namelengthmaxexceeded;
 end
