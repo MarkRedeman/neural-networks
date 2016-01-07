9899e4ddd0dcfea477805bccccb5528193479d9e
Mark Redeman <markredeman@gmail.com>
2016-01-07 13:32:08 +0100

    Gradient descent independent on amount of weights
    When plotting progress, show a bar graph of the current weights and show
    the min + max value of each weight.

--- matlab/+gradient/assignement_3.m
+++ matlab/+gradient/assignement_3.m
@@ -1,10 +1,13 @@
 %% assignement_3
-function [W] = assignement_3(samples, labels)
+function [W, trainingError, setError] = assignement_3(samples, labels)
 import gradient.gradient_descent;
 % Input dimension
 N = 50;
 
 % Training size
+P = 500;
+Q = 4500;
+
 P = 500;
 Q = 500;
 
@@ -12,10 +15,11 @@ Q = 500;
 M = length(labels);
 
 % Iterate tmax times over the training set
-tmax = 50;
+t_max = 500000;
 
 % Learning rate
 eta = 0.01;
+% eta = 0.05;
 
 % Permute input
 permutation = randperm(M);
@@ -23,21 +27,21 @@ permutation = randperm(M);
 permutedData = samples(permutation, :);
 permutedLabels = labels(permutation);
 
+hidden_nodes = 1;
+weights = normc(rand(hidden_nodes, N)); 
 % Start learning
-[W, trainingError, setError] = gradient_descent(permutedData, permutedLabels, ...
-                                                P, Q, eta, tmax);
+[W, trainingError, setError] = gradient_descent(weights, permutedData, permutedLabels, ...
+                                                P, Q, eta, t_max);
 
 figure;
 plot(trainingError)
 hold on
 plot(setError,'r')
 hold off
+set(gca, 'yscale', 'log')
 legend('training cost', 'test cost');
+title(sprintf('eta: %f, P: %d, Q: %d, t: %d', eta, P, Q, t_max));
 
 figure
-subplot(2, 1, 1)
-bar(W(1, :));
-subplot(2, 1, 2);
-bar(W(2, :));
-
+bar3(W);
 end
--- matlab/+gradient/gradient_descent.m
+++ matlab/+gradient/gradient_descent.m
@@ -1,18 +1,21 @@
 %% gradient_descent: iteration function used to update the weights of a
 %% neural network
-function [weights, cost_of_training, cost_of_test] = gradient_descent(samples, ...
-                                                      labels, training_size, testing_size, ...
-                                                      learning_rate, t_max, ...
-                                                      hidden_nodes)
-    if (nargin < 7)
-        hidden_nodes = 2;
-    end
-    
+function [weights, cost_of_training, cost_of_test, stats] = gradient_descent(weights, ...
+                                                      samples, ...
+                                                      labels,training_size, ...
+                                                      testing_size,learning_rate, ...
+                                                      t_max)
+    % if (nargout == 4)
+    %    stats = struct('training_set', [], 'testing_set', [], 'learning_rate', ...
+    %                   []
+    % end
+figure(randi([1, 256]));
+pause(0.1)
+    hidden_nodes = size(weights, 1); 
     dimension = size(samples, 2);
     cost_of_training = zeros(t_max, 1);
     cost_of_test = zeros(t_max, 1);
-
-    weights = normc(rand(hidden_nodes, dimension)); 
+    norms = zeros(hidden_nodes, t_max);
     
     training = 1 : training_size;
     testing = training_size + 1 : min(length(labels), training_size + 1 + testing_size);
@@ -28,11 +31,20 @@ function [weights, cost_of_training, cost_of_test] = gradient_descent(samples, .
 
             % Update the costs after (training_size) learning steps have been
             % done 
-                cost_of_training(t) = cost_function(samples(training, :), ...
+            if (mod(t, training_size) == 1)
+                t_i = ceil(t / (training_size)); 
+                cost_of_training(t_i) = cost_function(samples(training, :), ...
                                             labels(training), weights);
-                cost_of_test(t) = cost_function(samples(testing, :), ...
+                cost_of_test(t_i) = cost_function(samples(testing, :), ...
                                         labels(testing), weights);
+                norms(:, t_i) = min(weights, [], 2) + max(weights, [], 2);
+
+                plot_costs(cost_of_training, cost_of_test, learning_rate, ...
+                           training_size, testing_size, t, norms(:, 1 : t_i), weights);
+            end
     end
+    plot_costs(cost_of_training, cost_of_test, learning_rate, training_size, ...
+               testing_size, t, norms, weights);
 end
 
 %% jacobian: The jacobian to be used
@@ -54,12 +66,41 @@ end
 
 %% Cost function: computes E
 function [E] = cost_function(samples, labels, weights)
-    % W = weights';
-    % tau = labels';
-    % xi = samples';
-    % E = sum((ones(1, 2) * tanh(W' * xi) - tau).^2) / (2 * size(xi, 2));
    sample_size = length(labels);
    
    E = sum((sigma(samples, weights) - labels').^2) / (sample_size * 2);
 end
 
+
+function [handle] = plot_costs(cost_of_training, cost_of_test, learning_rate, training_size, ...
+                    testing_size, t, norms, weights)
+    subplot(2, 2, 1);
+    plot(cost_of_training)
+    hold on
+    plot(cost_of_test)
+    hold off
+    set(gca, 'YScale', 'log');
+    % set(gca, 'xScale', 'log');
+    xlabel('iterations');
+    legend('training cost', 'test cost');
+    title(sprintf('eta: %f, P: %d, Q: %d, t: %d', learning_rate, training_size, ...
+              testing_size, t));
+    subplot(2, 2, 2);
+    hold on;
+    for idx = 1 : size(norms, 1)
+        plot(norms(idx, :));
+    end
+    hold off
+    title('min + max of weights');
+    % set(gca, 'YScale', 'log');
+    % set(gca, 'xScale', 'log');
+    xlabel('iterations');
+
+subplot(2, 2, [3 4])
+bar(weights', 'grouped');
+xlim([0, size(weights, 2) + 1]);
+title('Size of component of each weight, where each color is a unique weight');
+% subplot(2, 2, 4);
+% bar(weights(2, :));
+    pause(1)
+end
