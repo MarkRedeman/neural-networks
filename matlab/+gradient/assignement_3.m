ccc73b8b4dc907b17daebd010dca629d125cdd50
Mark Redeman <markredeman@gmail.com>
2016-01-09 14:19:08 +0100

    Refactor the main assignment 3 file

--- matlab/+gradient/assignement_3.m
+++ matlab/+gradient/assignement_3.m
@@ -8,11 +8,8 @@ N = 50;
 P = 500;
 Q = 4500;
 
-P = 100;
-Q = 500;
-
-% Input size
-M = length(labels);
+% P = 500;
+% Q = 100;
 
 % Iterate tmax times over the training set
 t_max = 5000;
@@ -21,15 +18,17 @@ t_max = 5000;
 eta = 0.001;
 % eta = 0.05;
 
+%% Input size
+M = length(labels);
+
 % Permute input
 permutation = randperm(M);
-% permutation = 1 : M;
-permutedData = samples(permutation, :);
-permutedLabels = labels(permutation);
+samples = samples(permutation, :);
+labels = labels(permutation);
 
 hidden_nodes = 2;
-weights = normr(rand(hidden_nodes, N));
-weights(1, :) = - weights(2, :);
+weights = initial_weights(hidden_nodes, N);
+ 
 % Start learning
 training_size = P;
 testing_size = Q;
@@ -39,7 +38,15 @@ testing = training_size + 1 : min(length(labels), training_size + 1 + testing_si
 training_set = struct('samples', samples(training, :), 'labels', labels(training));
 testing_set = struct('samples', samples(testing, :), 'labels', labels(testing));
 
-[W, stats] = gradient_descent(weights, training_set, testing_set, eta, t_max);
-% gradient_descent(weights, training_set, testing_set, eta, t_max);
+plot_costs = true;
+[W, stats] = gradient_descent(weights, training_set, testing_set, eta, t_max, ...
+                              plot_costs);
 
+end
+
+function [weights] = initial_weights(hidden_nodes, dimension)
+    weights = normr(rand(hidden_nodes, dimension));
+    
+    % This might be an intersting initial condition
+    % weights(1, :) = - weights(2, :);
 end
